2df2bcd0aa5882eba3e470e2c52cec85179382b0
Thalles <thallesyam@gmail.com>
2022-07-26 12:24:10 -0300

    fix: path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
 const withImages = require('next-images')
 
+const alias = {
+  '@components': path.join(__dirname, 'src', 'components'),
+  '@styles': path.join(__dirname, 'src', 'styles'),
+}
+
 module.exports = withImages({
   exclude: path.resolve(__dirname, 'src/assets/svg'),
   images: {
@@ -8,6 +13,8 @@ module.exports = withImages({
   },
 
   webpack(config) {
+    config.resolve.alias = Object.assign({}, config.resolve.alias, alias)
+
     return config
   },
 })
